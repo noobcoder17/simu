d8d939aa315e85714a58e355e23f5df639ecffc1
Akash Debnath <debnathakash8@gmail.com>
2019-11-06 15:55:47 +0530

    new commit

--- hungarian.py
+++ hungarian.py
@@ -109,3 +109,94 @@ matrix = [
     [7, 12, 14, 10, 14]
 ]
 solveHungarian(matrix)
+
+
+
+
+#new 
+def reduce_row_column(mat,m,n):
+    for i in range(m):
+        k=min(mat[i])
+        if(k!=0):
+            for j in range(n):
+                mat[i][j]-=k
+    for j in range(n):
+        k=mat[0][j]
+        for i in range(1,m):
+            if(k>mat[i][j]):
+                k=mat[i][j]
+        for i in range(m):
+            mat[i][j]-=k
+
+def check_degeneracy(mat,m,n):
+    num_of_lines=0
+    rows=[False]*m
+    cols=[False]*n
+    temp=[]
+    for i in range(m):
+        for j in range(n):
+            if(mat[i][j]==0):
+                temp.append([i,j])
+    for k in temp:
+        i,j=k[0],k[1]
+        if(rows[i] or cols[j]): continue
+        count_r,count_c=0,0
+        for p in range(5):
+            if(mat[p][j]==0): count_c+=1
+            if(mat[i][p]==0): count_r+=1
+        if(count_r>=count_c): rows[i]=True
+        else: cols[j]=True
+        num_of_lines+=1
+    return num_of_lines,rows,cols
+
+def modify_matrix(mat,m,n,rows,cols):
+    k=1000000009
+    for i in range(m):
+        for j in range(n):
+            if(rows[i] or cols[j]): continue
+            if(mat[i][j]<k): k=mat[i][j]
+    for i in range(m):
+        for j in range(n):
+            if(rows[i]==True and cols[j]==True): mat[i][j]+=k
+            if(rows[i]==False and cols[j]==False ): mat[i][j]-=k
+
+arr=[[9,11,14,11,7],[6,15,13,13,10],[12,13,6,8,8],[11,9,10,12,9],[7,12,14,10,14]]
+mat=[[9,11,14,11,7],[6,15,13,13,10],[12,13,6,8,8],[11,9,10,12,9],[7,12,14,10,14]]
+m,n=5,5
+
+while(True):
+    reduce_row_column(mat,m,n)
+    lines,rows,cols=check_degeneracy(mat,m,n)
+    if(lines>=5): break
+    modify_matrix(mat,m,n,rows,cols)
+
+
+row=[0]*m
+col=[0]*n
+for i in range(m):
+    for j in range(n):
+        if(mat[i][j]==0):
+            row[i]+=1
+            col[j]+=1
+
+#print(row,col)
+assigned=[False]*m
+r=[False]*m
+cost=0
+for i in range(m):
+    for j in range(n):
+        if(row[i]==1 and col[j]==1 and mat[i][j]==0):
+            print(str(j+1),"assigned to",str(i+1))
+            cost+=arr[i][j]
+            assigned[j]=True
+
+for j in range(n):
+    if(assigned[j]):continue
+    for i in range(m):
+        if(mat[i][j]==0 and r[i]==False):
+            print(str(j+1),"assigned to",str(i+1))
+            r[i]=True
+            cost+=arr[i][j]
+            assigned[j]=True
+            break
+print(cost)
